c85e0e7bf32b78ff7b9c0adbd58c05c663bb09f7
Caleb Marhoover <cmarhoover@gmail.com>
2023-02-23 01:12:58 -0500

    Update fetch_model.py

--- fetch_model.py
+++ fetch_model.py
@@ -6,11 +6,13 @@ Requirements: msedgedriver.exe is in the same folder as this script.
 """
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 test_part_numbers=['98173A200', '7529K105', '93250A440']
 
-def fetch_model(part_numbers, delay=15):
+def fetch_model(part_numbers, delay=3):
     if type(part_numbers) is str:
         part_numbers=[part_numbers]
     
@@ -20,17 +22,19 @@ def fetch_model(part_numbers, delay=15):
     
     #For each part number
     for part_number in part_numbers:
-        driver.get('https://www.mcmaster.com/#' + str(part_number) + '/')
+        driver.get('https://www.mcmaster.com/' + str(part_number) + '/')
         #Pause for page to load
-        time.sleep(delay)    
-        #Find and Click submit button
+        time.sleep(10)    
+        #Find Download Dropdown and Select STEP
         try:
             try:
-                submit_button=driver.find_element_by_class_name("button-save--sidebyside")
-            except:
-                submit_button=driver.find_element_by_class_name("button-save--stacked")
+                select = Select(driver.find_element_by_xpath("//*[starts-with(@class, 'Dropdown_buttonDropdown')]"))
+                select.select_by_visible_text('3-D STEP')
+                submit_button = driver.find_element_by_xpath("//*[starts-with(@class, 'CadControl_downloadButton')]")
+            except NoSuchElementException:
+                time.sleep(delay)
             finally:
-                submit_button.click()
+                submit_button.click()        
         except:
             print('No button found or other error occured')
         finally:
